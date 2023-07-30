261a183e4aa1802e3ca886c9384b7e519957cb45
Felix Lidö <felix.lido@gmail.com>
2023-07-30 17:28:34 +0200

    Refactored player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,23 +2,22 @@
 //Rotation parts taken from ChatGPT
 
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Blaster blaster;
-    public float speed = 2.0f;
-    private const int MouseOneIndex = 0;
-
-    private Vector3 velocity;
     
-    public float rotationSpeed = 500f;
-    public float verticalRotationLimit = 80f; // Limit the camera's vertical rotation to prevent flipping
+    private const float Speed = 2.0f;
+    private const float Gravity = -9.8f;
+    private const float RotationSpeed = 500f;
+    private const float VerticalRotationLimit = 80f; // Limit the camera's vertical rotation to prevent flipping
+    private const int MouseOneIndex = 0;
+    
+    [SerializeField] private float  verticalRotation = 0f;
 
-    private float verticalRotation = 0f;
+    //------------------------------------------------------------------------------------------------------------------
     
     private void Start()
     {
@@ -26,60 +25,60 @@ public class PlayerMovement : MonoBehaviour
         blaster = GetComponentInChildren<Blaster>();
         Cursor.lockState = CursorLockMode.Locked;
     }
+    
+    //------------------------------------------------------------------------------------------------------------------
 
-    private void Update()
+    private void FixedUpdate()
     {
-
         Movement();
         Rotation();
         Shoot();
-        
     }
+    
+    //------------------------------------------------------------------------------------------------------------------
 
     private void Movement()
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-
+        
         Vector3 move = transform.right * x + transform.forward * y;
 
-        controller.Move(Time.deltaTime * speed * move);
+        controller.Move(Time.deltaTime * Speed * move);
+
+        if (!controller.isGrounded)
+        {
+            controller.Move(Vector3.down * MathF.Abs(Gravity) * Time.deltaTime);
+        }
+
     }
+    
+    //------------------------------------------------------------------------------------------------------------------
 
     private void Rotation()
     {
-        // Get the horizontal mouse input for rotation
         float horizontalRotationInput = Input.GetAxis("Mouse X");
-        // Get the vertical mouse input for camera rotation
         float verticalRotationInput = Input.GetAxis("Mouse Y");
 
-        // Calculate the rotation angles
-        float horizontalRotationAmount = horizontalRotationInput * rotationSpeed * Time.deltaTime;
-        verticalRotation -= verticalRotationInput * rotationSpeed * Time.deltaTime;
-        verticalRotation = Mathf.Clamp(verticalRotation, -verticalRotationLimit, verticalRotationLimit);
-
-        // Rotate the player and camera around the Y-axis
+        // Rotate player
+        float horizontalRotationAmount = horizontalRotationInput * RotationSpeed * Time.deltaTime;
+        verticalRotation -= verticalRotationInput * RotationSpeed * Time.deltaTime;
+        verticalRotation = Mathf.Clamp(verticalRotation, -VerticalRotationLimit, VerticalRotationLimit);
         transform.Rotate(0f, horizontalRotationAmount, 0f);
-
-        // Rotate the camera around the X-axis
-        // Get the current camera rotation angles
+        
+        //Rotate camera
         Vector3 cameraAngles = transform.GetChild(0).localEulerAngles;
-
-        // Apply the vertical rotation to the camera
         cameraAngles.x = verticalRotation;
-
-        // Zero out the camera's rotation around the Z-axis to prevent weird tilting
         cameraAngles.z = 0f;
-
-        // Apply the modified rotation angles to the camera
         transform.GetChild(0).localEulerAngles = cameraAngles;
     }
+    
+    //------------------------------------------------------------------------------------------------------------------
 
     private void Shoot()
     {
         if (Input.GetMouseButton(MouseOneIndex))
         {
-            Debug.Log("m1Pressed");
             blaster.Shoot();
         }
     }
